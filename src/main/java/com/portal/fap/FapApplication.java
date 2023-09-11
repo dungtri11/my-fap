92d39172b503b2f5450c0b3acd852ddb56fae3f8
DungNT <dungnthe176732@fpt.edu.vn>
2023-09-11 14:00:50 +0700

    create project

---
+++ src/main/java/com/portal/fap/FapApplication.java
@@ -0,0 +1,11 @@
+package com.portal.fap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FapApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FapApplication.class, args);
+	}
+}
---
+++ src/test/java/com/portal/fap/FapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.fap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
